76a53d6d97f64a2435a71695982688f6bda88f75
xb86 <135849218+xb86@users.noreply.github.com>
2025-01-22 19:35:47 +0100

    Update database.py

--- project/db/database.py
+++ project/db/database.py
@@ -1,5 +1,5 @@
 import psycopg2
-from config.settings import DATABASE_URL
+from config.settings import Settings.DATABASE_URL
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
